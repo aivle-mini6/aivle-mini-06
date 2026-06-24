1263727607e7a7cd13cc14252cde2e19d9f81b6b
wlgka <wlgka98@gmail.com>
2026-06-24 15:00:04 +0900

    에러 수정

--- backend/src/main/java/com/aivle/bookapp/controller/CommentController.java
+++ backend/src/main/java/com/aivle/bookapp/controller/CommentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/books/{bookId}/comments")
+@RequestMapping("/{bookId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
 

7db4c67bab3b9054449be4fcb7ba723c56754772
LimHHSS <abc96417@naver.com>
2026-06-24 13:02:17 +0900

    Route book API through EC2 nginx proxy

--- deploy-scripts/application-start-hook.sh
+++ deploy-scripts/application-start-hook.sh
@@ -24,6 +24,14 @@ server {
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
     }
+
+    location /books {
+        proxy_pass http://127.0.0.1:8080/books;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
 }
 NGINX
 
